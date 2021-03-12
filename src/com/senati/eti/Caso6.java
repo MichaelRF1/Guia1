f54ce471207984cbade342d05c7d7f3e582d42a0
Michael <Michael@LAPTOP-T66AOFLG>
2021-03-12 16:03:20 -0600

    Proyecto parcial al 12/03/21

---
+++ src/com/senati/eti/Caso6.java
@@ -0,0 +1,21 @@
+package com.senati.eti;
+
+public class Caso6 {
+
+	public static void main(String[] args) {
+		int rp1 = (int) Math.pow(5, 3);
+		
+		float rp2 = (float) Math.pow(81, 1/2.0);
+		
+		float rp3 = (float) Math.pow(256, 1/4.0);
+		
+		float rp4 = (float) Math.pow(25, 1/2.0);
+		
+		System.out.println("======Valores Ingresados======");
+		System.out.println("Resultado 1...: " + rp1);
+		System.out.println("Resultado 2...: " + rp2);
+		System.out.println("Resultado 2...: " + rp3);
+		System.out.println("Resultado 2...: " + rp4);
+	}
+
+}
